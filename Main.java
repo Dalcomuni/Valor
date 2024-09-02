1e9fca18760f88f42ab18f6cd37173bd08dbc99c
Ricardo Dalcomuni <161725206+Dalcomuni@users.noreply.github.com>
2024-09-02 11:19:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in );
+    System.out.println("digite um valor:");
+     double A = scanner.nextDouble();
+    System.out.println("digite um valor:");
+    double B = scanner.nextDouble();
+    System.out.println("digite um valor:");
+    double C = scanner.nextDouble();
+    
+
+
+    
+  }
+}
